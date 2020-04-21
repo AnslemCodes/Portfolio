5ea42264d915ce77e9368ba28de585b41d0da37a
ansroid <ansledozie@gmail.com>
2020-04-21 12:18:57 +0100

    implemented intent and share button

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.portfolioapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Message
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.ShareActionProvider
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.MenuItemCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         //since we've remove the default action bar, implement the toolbar to show on our action bar
         val toolbar =
             findViewById<Toolbar>(R.id.toolbar)
+        findViewById<Toolbar>(R.id.share_item)
         setSupportActionBar(toolbar)
 
 
@@ -24,6 +27,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.toolbar_menu, menu)
+        val menuItem = menu.findItem(R.id.share_item)
+
         return super.onCreateOptionsMenu(menu)
+
+
+
     }
+
+    //setting on click listener on toolbar menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        if (item.itemId ==R.id.share_item){
+            val intent = Intent ()
+            intent.action = Intent.ACTION_SEND
+            val message : String = etUserMessage.text.toString()
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+        }
+
+        if (item.itemId == R.id.refresh) {
+            Toast.makeText(this, "Refresh Activity", Toast.LENGTH_LONG).show()
+        }
+        if (item.itemId == R.id.settings) {
+            Toast.makeText(this, "Settings Activity", Toast.LENGTH_LONG).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
